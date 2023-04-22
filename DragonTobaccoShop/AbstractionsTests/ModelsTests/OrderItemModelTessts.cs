2b8fb3ac4db414d4eb43abecaa1f8cb6050624cd
Vladyslav <onekryvenko.vladyslav@gmail.com>
2023-04-22 21:02:50 +0300

    Added test for OrderItem

---
+++ DragonTobaccoShop/AbstractionsTests/ModelsTests/OrderItemModelTessts.cs
@@ -0,0 +1,22 @@
+﻿using Abstractions.Models;
+using AbstractionsTests.Common;
+using Xunit;
+
+namespace AbstractionsTests.ModelsTests
+{
+    public class OrderItemModelTessts : BaseTests
+    {
+        [Theory]
+        [InlineData("Count")]
+        public void OrderItemHasIntegerPropertyCount(string count)
+        {
+            var targetType = typeof(IOrderItem<>);
+
+            var propertyType = typeof(int);
+
+            var result = TestsHelper.TypeHasPropertyWithRequiredTypeAndName(targetType, propertyType, count);
+
+            Assert.True(result);
+        }
+    }
+}
