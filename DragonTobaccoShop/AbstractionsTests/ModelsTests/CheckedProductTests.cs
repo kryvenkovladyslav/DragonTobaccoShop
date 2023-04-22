6cd56c63cdff08fe277be17c7c70f0ba11808035
Vladyslav <onekryvenko.vladyslav@gmail.com>
2023-04-22 20:33:36 +0300

    Added tests for CheckedProduct and WithLst

---
+++ DragonTobaccoShop/AbstractionsTests/ModelsTests/CheckedProductTests.cs
@@ -0,0 +1,4 @@
+﻿namespace AbstractionsTests.ModelsTests
+{
+    internal class CheckedProductTests : BaseTests { }
+}
---
+++ DragonTobaccoShop/AbstractionsTests/ModelsTests/WithListModelTests.cs
@@ -0,0 +1,4 @@
+﻿namespace AbstractionsTests.ModelsTests
+{
+    public class WithListModelTests : BaseTests { }
+}
