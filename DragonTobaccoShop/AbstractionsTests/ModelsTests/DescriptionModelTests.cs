b016eff8f9fc4124f151a2649e662f99c801e67b
Vladyslav <onekryvenko.vladyslav@gmail.com>
2023-04-22 20:40:33 +0300

    Added test for Description Model

---
+++ DragonTobaccoShop/AbstractionsTests/ModelsTests/DescriptionModelTests.cs
@@ -0,0 +1,19 @@
+﻿using Abstractions.Models;
+using System.Linq;
+using Xunit;
+
+namespace AbstractionsTests.ModelsTests
+{
+    public class DescriptionModelTests : BaseTests
+    {
+        [Theory]
+        [InlineData("Path")]
+        public void TypeHasStringPropertyPath(string path)
+        {
+            var requiredProperty = typeof(IDesctiptionModel<>).GetProperties()
+                .Where(p => p.PropertyType == typeof(string) && p.Name == path);
+
+            Assert.NotNull(requiredProperty);
+        }
+    }
+}
