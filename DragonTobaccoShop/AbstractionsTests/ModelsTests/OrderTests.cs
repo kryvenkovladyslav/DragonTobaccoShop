fa49b9a5c11795063e1edec91e6eb3c313f307a4
Vladyslav <onekryvenko.vladyslav@gmail.com>
2023-04-22 21:17:05 +0300

    Added test for Order Model

---
+++ DragonTobaccoShop/AbstractionsTests/ModelsTests/OrderTests.cs
@@ -0,0 +1,35 @@
+﻿using Abstractions.Models;
+using AbstractionsTests.Common;
+using Xunit;
+
+namespace AbstractionsTests.ModelsTests
+{
+    public sealed class OrderTests : BaseTests
+    {
+        [Theory]
+        [InlineData("Status")]
+        public void OrderItemHasStringPropertyStatus(string propertyName)
+        {
+            var targetType = typeof(IOrderModel<>);
+
+            var propertyType = typeof(string);
+
+            var result = TestsHelper.TypeHasPropertyWithRequiredTypeAndName(targetType, propertyType, propertyName);
+
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData("TototalPrice")]
+        public void OrderItemHasDeciamlPropertyTototalPrice(string propertyName)
+        {
+            var targetType = typeof(IOrderModel<>);
+
+            var propertyType = typeof(decimal);
+
+            var result = TestsHelper.TypeHasPropertyWithRequiredTypeAndName(targetType, propertyType, propertyName);
+
+            Assert.True(result);
+        }
+    }
+}
