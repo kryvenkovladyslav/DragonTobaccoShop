8910c3c03c7924178b0e3c4ffbc6245bb07b87a0
Vladyslav <onekryvenko.vladyslav@gmail.com>
2023-04-22 20:30:35 +0300

    Added test for CartSessionModel

---
+++ DragonTobaccoShop/AbstractionsTests/ModelsTests/BaseTests.cs
@@ -0,0 +1,45 @@
+﻿using Abstractions.Models;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace AbstractionsTests.ModelsTests
+{
+    public  class BaseTests
+    {
+        [Fact]
+        public virtual void IsGenericType()
+        {
+            Assert.True(typeof(ICartSessionModel<>).IsGenericType);
+        }
+
+        [Fact]
+        public virtual void TypeHasValueTypeConstraint()
+        {
+            var typeConstraint = typeof(ICartSessionModel<>).GetGenericArguments().First()
+                .GetGenericParameterConstraints().FirstOrDefault();
+
+            Assert.Equal(typeof(ValueType), typeConstraint);
+        }
+
+        [Theory]
+        [InlineData("ID")]
+        public virtual void TypeHasPropertyID(string id)
+        {
+            var requiredProperty = typeof(ICartSessionModel<>).GetProperties()
+                .Where(p => p.Name == id).FirstOrDefault();
+
+            Assert.Contains(id, requiredProperty?.Name);
+        }
+
+        [Theory]
+        [InlineData("ID")]
+        public virtual void TypeHasGenericPropertyID(string id)
+        {
+            var requiredProperty = typeof(ICartSessionModel<>).GetProperties()
+                .Where(p => p.PropertyType.IsGenericTypeParameter && p.Name == id).FirstOrDefault();
+
+            Assert.True(requiredProperty != null);
+        }
+    }
+}
---
+++ DragonTobaccoShop/AbstractionsTests/ModelsTests/CartSessionModelTests.cs
@@ -0,0 +1,4 @@
+﻿namespace AbstractionsTests.ModelsTests
+{
+    public class CartSessionModelTests : BaseTests { }
+}
