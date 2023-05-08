46a541060a3c3e12b610ee8d3b3c81bc04376a52
DESKTOP-1UR1491\Vladyslav <onekryvenko.vladyslav@gmail.com>
2023-05-08 12:01:45 +0300

    Added dependency injection

---
+++ DragonTobaccoShop/DataAccessLayer/DependencyInjection/DependencyInjectionExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Abstractions.DataAccessLayer.Repository;
+using DataAccessLayer.Common.Repository;
+using Domain.ApplicationModels;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace DataAccessLayer.DependencyInjection
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static void TryAddScopedRepository<TEntity>(this IServiceCollection services) where TEntity : class
+        {
+            services.TryAddScoped<IRepository<TEntity>, Repository<TEntity>>();
+        }
+        
+        public static void TryAddScopedAllApplicationRepositories(this IServiceCollection services)
+        {
+            services.TryAddScopedRepository<ManufacturerDescription>();
+            services.TryAddScopedRepository<TobaccoDescription>();
+            services.TryAddScopedRepository<CheckedProduct>();
+            services.TryAddScopedRepository<Manufacturer>();
+            services.TryAddScopedRepository<Order>();
+            services.TryAddScopedRepository<OrderItem>();
+            services.TryAddScopedRepository<Product>();
+            services.TryAddScopedRepository<Review>();
+            services.TryAddScopedRepository<Role>();
+            services.TryAddScopedRepository<Strength>();
+            services.TryAddScopedRepository<Taste>();
+            services.TryAddScopedRepository<Tobacco>();
+            services.TryAddScopedRepository<TobaccosTastes>();
+            services.TryAddScopedRepository<User>();
+            services.TryAddScopedRepository<UsersRoles>();
+            services.TryAddScopedRepository<WishList>();
+            services.TryAddScopedRepository<CartSession>();
+        }
+    }
+}
